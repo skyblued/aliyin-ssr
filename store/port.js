3866ba3f659ac8208df78c3e3e122a189d29916f
skyblued <806020149@qq.com>
2019-06-21 19:36:49 +0800

    2019年6月21日19:36:48

--- store/port.js
+++ store/port.js
@@ -9,4 +9,41 @@ export const state = () => ({
 	productThumbnail: 'http://node.aliyin.com/svgtopng', // 生成缩略图地址
 	preveiwPort: 'http://node.aliyin.com/replace', // 生成预览图http://localhost:5050/replace
 	textToSvg: 'http://open.aliyin.com/api/TextToSvg',
+
+
+
+	UploadToOSS: '/UploadToOSS', // 图片上传
+	TextToSvg: '/TextToSvg', // 文字生成path
+	MaterialTypes: '/MaterialTypes', // 素材类型
+	MaterialCategories: '/MaterialCategories', // 素材分类标签
+	MaterialInfos: '/MaterialInfos', // 素材列表
+	imgBaseUrl: 'http://img.aliyin.com/', // 阿里OS
+
+	/* 设计器  ↓ */
+	// 团队保存, 更新,创建接口
+	TeamMaterial: '/TeamMaterial', // 团队上传素材
+	TeamMaterials: '/TeamMaterials', // 获取团队素材列表
+	AllTemplate: '/AllTemplateClassification', // 获取首页全部模板分类
+	SetUpTemplate: '/SetUpTemplate', //团队创建的模板列表
+	TeamTemplate: '/TeamTemplate', // 获取团队模板详情
+	TeamMaterials: '/TeamMaterials', // 用户团队自己上传的所有素材列表
+	DocumentPage: '/DocumentPage', // 团队更新模板页
+	DocumentPageSort: '/DocumentPageSort', // 团队模板页调顺序
+	Document: '/Document', // 获取团队模板
+	TeamMasters: '/TeamMasters', // 获取团队母版
+	FavoritedTemplates: '/FavoritedTemplates', // 获取团队(个人)收藏模板
+
+	// 设计师保存,创建接口, 更新
+	Template: '/Template', // 获取公用模板
+	DesignerTemplate: "/DesignerTemplate", // 设计师保存模板
+	DesignerTemplatePage: '/DesignerTemplatePage', //设计师更新和添加页,修改页
+	TemplatePageSort: '/TemplatePageSort', // 页面调顺序
+	DesignerMaterial: '/DesignerMaterial', // 素材
+	DesignerMaterials: '/DesignerMaterials', // 获取上传列表get
+	/*设计器 ↑ */
+
+	// 模板: 获取当前产品类型的所有页
+	Templates: '/Templates',
+	// 颜色: 团队专属颜色获取
+	TeamColors: '/TeamColors',
 })
