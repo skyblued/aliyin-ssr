96bd389b5f39127c505bd93ff7367d6311638ce8
skyblued <806020149@qq.com>
2019-06-21 20:37:22 +0800

    2019-6-21 20:37:20

---
+++ pages/design.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'design',
+	async asyncData({$axios, isDev, route, store, env, params, query, req, res, redirect, error}) {
+		let {data} = await $axios.get("/Fonts");
+		return {
+			fontList: data
+		}
+		
+	},
+	mounted() {
+		console.log(this.fontList)
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
