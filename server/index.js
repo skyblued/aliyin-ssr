854424252376ea79c6548736cb8668961bebc3ea
skyblued <806020149@qq.com>
2019-06-24 18:29:45 +0800

    2019年6月24日18:29:44

--- server/index.js
+++ server/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-const request = require('request')
 const app = express()
-// console.log(request)
-app.prototype.request = request;
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
