0f73de451271122f09d1842c50f90b547b98a7cc
wuzhao <846343090@qq.com>
2019-06-26 18:40:11 +0800

    2019年6月26日18:40:10

--- pages/print.vue
+++ pages/print.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
 		<HeaderTop></HeaderTop>
-		<Header></Header>
+		<Header id="header" :isFixed="isFixed" :barList="barList" :class="{fixed : isFixed}"></Header>
 		<nuxt-child></nuxt-child>
 		<ToolsBar></ToolsBar>
 		<Footer :qrcode="qrcode" :articleList="articleList"></Footer>
@@ -22,14 +22,36 @@ export default {
 		ToolsBar
     },
     async asyncData({$axios, isDev,route, store, env, params, query, req, res, redirect, error}) {
-		let [FooterCode, FooterArticle] = await Promise.all([
+		let [AllTemplate, FooterCode, FooterArticle] = await Promise.all([
+			$axios.get(store.state.port.AllTemplate),
 			$axios.get(store.state.port.Advertise + '?ID=136'),
 			$axios.get('/Articles')
 		])
 		return {
+			barList: AllTemplate.data,
 			qrcode: FooterCode.data.Advertisements[0].FileUrl,
-			articleList: FooterArticle.data
+			articleList: FooterArticle.data,
+			isFixed: false,
 		}
+	},
+	methods: {
+		scrollToTop() {
+			let osTop = document.documentElement.scrollTop || document.body.scrollTop
+			if (osTop >= 40) {
+				this.isFixed = true
+				this.marginTop = document.querySelector('#header').offsetHeight + 'px'
+			} else {
+				this.isFixed = false
+				this.marginTop = 0
+			}
+		}
+	},
+	mounted() {
+		this.$store.commit('login/addToKen', localStorage['token']);
+		window.addEventListener('scroll', this.scrollToTop)
+	},
+	destroyed () {
+		window.removeEventListener('scroll', this.scrollToTop); 
 	},
 }
 </script>
