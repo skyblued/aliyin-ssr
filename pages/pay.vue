ae6cf8a8f5ea8b0b8ad5d7bfd0f16120c93f97a0
skyblued <806020149@qq.com>
2019-07-05 16:46:48 +0800

    2019年7月5日16:46:47

--- pages/pay.vue
+++ pages/pay.vue
@@ -6,35 +6,17 @@
 
 <script>
 export default {
-    data () {
-        // let params = this.$route.params.t
-        // params = params.split('&')
-        // console.log(params)
-        return {
-            orderCode: '',
-            active: '',
-            num: 0,
-            teamNum: '',
-            userId: ''
-        }
-    },
+	asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+			console.log(query)
+			return {
+				orderCode: query.c,
+				active: query.a,
+				num: query.n,
+				teamNum: query.t,
+				userId: query.u
+			}
+	},
     mounted() {
-        var url = window.location.href
-        // console.log(url)
-        var str = url.split('/')[4]
-        var msg = str.split('&')
-        var m = []
-        for(var i=0;i<msg.length;i++) {
-            m.push(msg[i].substr(2))
-        }
-        // console.log(m)
-        this.orderCode = m[0]
-        this.active = m[2]
-        this.teamNum = m[1]
-        this.userId = m[4]
-
-
-
         var ua = window.navigator.userAgent.toLowerCase();
         console.log(ua)
         //判断是不是微信
